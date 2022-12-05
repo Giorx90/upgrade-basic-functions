5d00535e63c16d21ad8e313a3f4902daa59c1b4c
Jorge M <j.maldo.1990@gmail.com>
2022-12-05 20:42:13 +0100

    Iteration#1

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo) {
+        return numberOne;
+    } else 
+        return numberTwo;
+    }
+
+let numeroMayor = sum(6,2)
+console.log(numeroMayor)
+
---
+++ Ejercicio2.js
@@ -0,0 +1,13 @@
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(param) {
+    for (i=0; i < avengers.length; i++) {
+        let longestWord = 0
+        i = avengers[i]
+        if (avengers[i] > longestoWord) {
+            longestword + avengers[i].length
+        }  
+    }
+
+}
+
+console.log(findLongestWord(avengers))
