8a83f6d9e40f3a688e124bf6750430a3e27f35ce
Jorge M <j.maldo.1990@gmail.com>
2022-12-08 09:31:43 +0100

    Iteration#2

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,13 +1,15 @@
 const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
-function findLongestWord(param) {
-    for (i=0; i < avengers.length; i++) {
-        let longestWord = 0
-        i = avengers[i]
-        if (avengers[i] > longestoWord) {
-            longestword + avengers[i].length
-        }  
-    }
 
+function findLongestWord(array) {
+    let longestWord = "";
+    for(let i = 0; i < array.length; i++) {
+        if (longestWord.length < array[i].length) {
+            longestWord = array[i];
+        } else {
+            i++
+        }
+    }
+    return longestWord
 }
 
 console.log(findLongestWord(avengers))
